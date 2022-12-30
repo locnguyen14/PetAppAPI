90b7184e4d477a1a07006b1d232bb13ec569c4e1
LocNguyen <LocNguyen@SG-DELL-18>
2022-12-30 11:55:13 -0500

    Create custom choice field for serializer

--- animal/serializers.py
+++ animal/serializers.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from .models import Animal
 from rest_framework import serializers
 
+from utils.utils import ChoiceField
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -11,6 +13,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
 class AnimalSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    animalType = ChoiceField(choices=Animal.ANIMAL_TYPES)
 
     class Meta:
         model = Animal
