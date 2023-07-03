8a26407f23ebdabbf89ed466914b69fa44d9425a
Loc Nguyen <ndxloc1995@gmail.com>
2023-07-03 14:29:20 -0400

    update connection to database for github action

--- app/API/settings.py
+++ app/API/settings.py
@@ -131,11 +131,11 @@ if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
         'default': {
            'ENGINE': 'django.db.backends.postgresql',
-           'NAME': env("DB_NAME"),
-           'USER': env("DB_USER"),
-           'PASSWORD': env("DB_PASSWORD"),
-           'HOST': env("DB_HOST"),
-           'PORT': env("DB_PORT"),
+           'NAME': 'devdb',
+           'USER': 'devuser',
+           'PASSWORD': 'changeme',
+           'HOST': 'postgres',
+           'PORT': '5432',
         }
     }
 
